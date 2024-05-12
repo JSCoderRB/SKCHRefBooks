214d873f3e5022157fd6b73a88736c984ff0cdeb
Gokul Kamat <gokulgkamat@proton.me>
2024-05-12 20:27:08 +0530

    Move auth-related app routes to separate file

--- main.py
+++ main.py
@@ -1,96 +1,14 @@
 # Imports
 from flask import Flask, render_template, request, redirect, url_for, flash, session
-from forms import LoginForm, SignupForm
+from auth import auth_bp
 import pandas as pd
 import os
 import time
-import backend as bk
 
 
 # Initializing a Flask object for our app
 app = Flask(__name__, template_folder="html")
-
-
-# Login route definition
-@app.route("/", methods=["POST", "GET"])
-def login():
-    df = app.config["users_df"]
-    uploads = app.config["uploads_df"].to_dict(orient="records")
-    emails = df.get("email").tolist()
-    form = LoginForm()
-
-    # Run the following only if the form details are valid
-    if form.validate_on_submit():
-        # Check if the user exists
-        if form.email.data not in emails:
-            flash("Invalid username", "error")
-            return redirect(url_for("login"))
-
-        # Retrieve user's data from the dataframe
-        userdata = bk.get_userdata(df, form.email.data)
-
-        # Check if password is correct
-        if form.password.data != userdata["password"]:
-            flash("Invalid password", "error")
-            return redirect(url_for("login"))
-
-        # Store the details in session
-        session["details"] = userdata
-
-        # Redirect to the user dashboard
-        return redirect(url_for("dashboard"))
-
-    return render_template("login.html", form=form, uploads=uploads)
-
-
-# Sign Up route definition
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    emails = app.config["users_df"].get("email").tolist()
-    form = SignupForm()
-
-    # Run the following only if the form details are valid
-    if form.validate_on_submit():
-        # Retrieve all the signup form data
-        email = request.form["email"]
-        name = request.form["name"]
-        grade = request.form["grade"]
-        contact_number = request.form["contact_number"]
-        address = request.form["address"] if "address" in request.form else None
-        password = request.form["password"]
-
-        # Check if username already exists
-        if email in emails:
-            flash("Username exists, login from the Login Page...", "success")
-            time.sleep(3)
-            return render_template("redirect.html", redirect_url="/")
-
-        # Structure the user's details
-        userDetails = [
-            email,
-            password,
-            name,
-            grade,
-            contact_number,
-            address,
-        ]
-
-        fields = ["email", "password", "name", "grade", "contact_number", "address"]
-
-        # Update the new user as a row in the dataframe
-        app.config["users_df"] = bk.insert_row(
-            app.config["users_df"], userDetails, fields
-        )
-
-        # Write the dataframe to csv file
-        app.config["users_df"].to_csv(app.config["users_file"], index=False)
-
-        # Show a message and redirect to login page
-        flash("Signup successful! Redirecting to login page...", "success")
-        time.sleep(3)
-        return render_template("redirect.html", redirect_url="/")
-
-    return render_template("signup.html", form=form)
+app.register_blueprint(auth_bp)
 
 
 # Dashboard route definition
@@ -172,13 +90,6 @@ def book_request():
             return render_template("redirect.html", redirect_url="/dashboard")
 
         return redirect(url_for("dashboard"))
-
-
-# Log out route definition
-@app.route("/logout")
-def logout():
-    session.pop("details", None)  # to clear the session data
-    return redirect("/")
 
 
 # Main
